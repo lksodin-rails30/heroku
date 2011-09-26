be1f509aca110f3edfce4588bde2a759661faecb
lksodin-rails30 <a5272107@nepwk.com>
2011-09-26 15:53:39 +0800

    test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Member::Application.config.secret_token = '32899c532ae3b5b60a9653f3931022b21ef36d8beb09dff3bccf325890a128717327d4fdac9db079ff6960d94785a0b8e2134b6d672721de2634fd73b239654e'
